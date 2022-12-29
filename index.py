7606a76a620dedc9d860df866de2b527f05349ff
ali <alidashti1780@gmail.com>
2022-12-29 19:14:39 +0330

    Initial commit

---
+++ index.py
@@ -0,0 +1,19 @@
+import shlex
+
+# این متغییر همان دیتابیس ماست
+DATA = {}
+
+
+while True:
+    try:
+        cmd, *args = shlex.split(input('# '))
+    except ValueError as e:
+        continue
+    except KeyboardInterrupt as e:
+        print('Bye!')
+        break
+    
+    if cmd.upper() == 'SET':
+        DATA[args[0]] = args[1]
+    elif cmd.upper() == 'GET':
+        print(DATA[args[0]])
