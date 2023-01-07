98174d0ec017b2391f3a87db529ceb1b0a00f2aa
Nazila <103484801+Nazila001@users.noreply.github.com>
2023-01-07 23:48:18 +0330

    upgrade and create new changes about input

--- index.py
+++ index.py
@@ -2,29 +2,34 @@ import shlex
 
 # این متغییر همان دیتابیس ماست
 DATA = {}
+num_test = 0
 
 
 def checking_input(x):
-    if type(int(x)) is not int:
-        raise TypeError('Work with Numbers Only')
-
-
-def cntr_arg():
-    ...
-
-
+    if len(args) > 2:
+        if args[1] == 'int':
+            if type(int(args[2])) != type(num_test):
+                print("Please enter the correct form of value!")
+            else:
+                args[1] = args[2]
+                del args[2]
+        elif args[1] == 'str':
+            print('Work with Numbers Only')
+    else:
+        if type(int(1)) is not int:
+            raise TypeError('Work with Numbers Only')
+            
 while True:
     try:
         cmd, *args = shlex.split(input('# '))
         if cmd == 'set':
             if args[0] not in DATA and len(args) < 3:
-                print("Please enter the type of value!")
+                print("Please enter the correct form of value!")
                 break
             if args[0] not in DATA:
-                checking_input(args[2])
-                args[1] = args[2]
-                del args[2]
-            checking_input(args[1])
+                checking_input(args)
+            else:
+                checking_input(args)
 
     except ValueError:
         print("Oops!  That was no valid number.  Try again...")
